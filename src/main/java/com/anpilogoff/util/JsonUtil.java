610c5ac50d3596d0428a74e12f72a8e6285de2f4
manpilogoff <anpilogov.dev@proton.me>
2025-07-23 08:37:30 +0300

    Edited:
     -HttpUtil: добавлены 2 метода
       *checkSearchResponseValidWithAuthToken() - проверяет Response Body length
       *checkTokenValidForDownload() - проверяет "Content-Length"
       **оба нужны для правильной подстановки AUTH..TOKEN в запрос и дальнейшего избежания пустого ответа
        на /search и неполного(preview 0:30) аудиотрека при скачивании.
    
     JsonUtil: добавлен метод для извлечения нужных данных из ответа на /search

--- src/main/java/com/anpilogoff/util/JsonUtil.java
+++ src/main/java/com/anpilogoff/util/JsonUtil.java
@@ -5,21 +5,23 @@ import com.anpilogoff.database.entity.Track;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.concurrent.CompletionException;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
+@Slf4j
 public class JsonUtil {
-    private static final Logger log = LoggerFactory.getLogger(JsonUtil.class);
     public static final ObjectMapper MAPPER = new ObjectMapper();
 
     public static JsonNode parseJson(String str) {
         try {
             return MAPPER.readTree(str);
-        }
-        catch (JsonProcessingException e) {
+        } catch (JsonProcessingException e) {
             throw new CompletionException(new RuntimeException("JSONfailed: " + e.getMessage()));
         }
     }
@@ -27,7 +29,6 @@ public class JsonUtil {
     public static List<Track> extractTracks(JsonNode tracksJsonArray, Album album) {
         List<Track> tracks = new ArrayList<>();
         if (tracksJsonArray.isArray()) {
-            log.info(("         [<Album>] : ".concat( album.getTitle())));
 
             for (JsonNode trackNode : tracksJsonArray) {
                 Track track = Track.builder()
@@ -50,6 +51,45 @@ public class JsonUtil {
 
     public static String extractTrackUrl(String textJson) {
         return parseJson(textJson).path("url").asText().replace("\\","");
+    }
+
+
+    public static String extractSearch(String textJson)  {
+        JsonNode root = null;
+        String tracksString = null;
+
+        try {
+            root = MAPPER.readTree(textJson);
+
+            List<Map<String, Object>> filteredTracks = StreamSupport.stream(
+                            root.path("tracks").path("items").spliterator(), false)
+                    .map(track -> {
+                        Map<String, Object> trackMap = new LinkedHashMap<>();
+                        trackMap.put("id", track.path("id").asText());
+                        trackMap.put("title", track.path("title").asText());
+
+                        Map<String, Object> artist = new LinkedHashMap<>();
+                        artist.put("id", track.path("performer").path("id").asText());
+                        artist.put("name", track.path("performer").path("name").asText());
+                        trackMap.put("artist", artist);
+
+                        Map<String, Object> album = new LinkedHashMap<>();
+                        album.put("title", track.path("album").path("title").asText());
+                        album.put("qobuz_id", track.path("album").path("qobuz_id").asText());
+                        album.put("tracks_count", track.path("album").path("tracks_count").asInt());
+                        album.put("genre_id", track.path("album").path("genre").path("id").asText());
+                        trackMap.put("album", album);
+
+                        return trackMap;
+                    }).collect(Collectors.toList());
+
+            tracksString = MAPPER.writeValueAsString(filteredTracks);
+
+        } catch (JsonProcessingException e) {
+            log.error("Json parsing failed {}", e.getMessage(),e);
+            return null;
+        }
+        return tracksString;
     }
 
 
